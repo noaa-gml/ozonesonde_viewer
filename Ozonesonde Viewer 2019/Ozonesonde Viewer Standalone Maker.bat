1df95ca78c06c6799210c0b5f7a36ea37cc326a1
Allen Jordan <allen.jordan@noaa.gov>
2019-12-03 15:48:30 -0700

    Updated signature check path

--- Ozonesonde Viewer 2019/Ozonesonde Viewer Standalone Maker.bat
+++ Ozonesonde Viewer 2019/Ozonesonde Viewer Standalone Maker.bat
@@ -9,7 +9,7 @@ REM ############################################################################
 
 REM read the version number of the executable, uses code from http://stackoverflow.com/questions/6697878/get-the-file-version-of-a-dll-or-exe
 REM note that this depends on the cmd line utility "sigcheck", check above link for download
-FOR /F "tokens=1-3" %%i IN ('c:\cmd_utilities\sigcheck.exe "%execPath%\%execName%"') DO ( IF "%%i %%j"=="File version:" SET filever=%%k )
+FOR /F "tokens=1-3" %%i IN ('C:\Users\jordan\cmd_tools\sigcheck.exe "%execPath%\%execName%"') DO ( IF "%%i %%j"=="File version:" SET filever=%%k )
 
 REM strip trailing space
 set filever=%filever:~0,-1%
